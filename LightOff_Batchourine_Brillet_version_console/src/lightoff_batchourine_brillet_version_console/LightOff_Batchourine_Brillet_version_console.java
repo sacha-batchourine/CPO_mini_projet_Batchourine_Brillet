3e559c9130b1f23229a02991733eb36d58dfb1c7
sachabatchourine <sachabatchourine@localhost>
2024-11-20 10:37:46 +0100

    projet

---
+++ LightOff_Batchourine_Brillet_version_console/src/lightoff_batchourine_brillet_version_console/LightOff_Batchourine_Brillet_version_console.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_batchourine_brillet_version_console;
+
+/**
+ *
+ * @author sachabatchourine
+ */
+public class LightOff_Batchourine_Brillet_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
