4933b9d80d1a9b86f1e1cfc22ce0816ef163dfe7
sachabatchourine <sachabatchourine@localhost>
2024-11-20 11:28:15 +0100

    projet

--- LightOff_BATCHOURINE_BRILLET_version_console/src/lightoff_batchourine_brillet_version_console/CelluleLumineuse.java
+++ LightOff_BATCHOURINE_BRILLET_version_console/src/lightoff_batchourine_brillet_version_console/CelluleLumineuse.java
@@ -9,6 +9,71 @@ package lightoff_batchourine_brillet_version_console;
  *
  * @author sachabatchourine
  */
+/**
+ * La classe CelluleLumineuse représente une cellule lumineuse
+ * qui peut être dans l'état "allumée" ou "éteinte".
+ * Elle offre des méthodes pour gérer son état.
+ */
 public class CelluleLumineuse {
-    
+
+    /**
+     * Attribut représentant l'état de la cellule.
+     * true : allumée, false : éteinte.
+     */
+    private boolean etat;
+
+    /**
+     * Constructeur par défaut.
+     * Initialise l'état de la cellule à "éteinte" (false).
+     */
+    public CelluleLumineuse() {
+        this.etat = false; // État initial éteint
+    }
+
+    /**
+     * Active ou désactive la cellule lumineuse.
+     * Si la cellule est allumée, elle devient éteinte.
+     * Si la cellule est éteinte, elle devient allumée.
+     */
+    public void activerCellule() {
+        this.etat = !this.etat; // Inverse l'état
+    }
+
+    /**
+     * Éteint la cellule lumineuse.
+     * Si la cellule est déjà éteinte, cette méthode n'a aucun effet.
+     */
+    public void eteindreCellule() {
+        this.etat = false; // Force l'état à éteint
+    }
+
+    /**
+     * Vérifie si la cellule est éteinte.
+     *
+     * @return true si la cellule est éteinte, false sinon.
+     */
+    public boolean estEteint() {
+        return !this.etat; // Retourne true si éteinte
+    }
+
+    /**
+     * Renvoie l'état actuel de la cellule.
+     *
+     * @return true si la cellule est allumée, false si elle est éteinte.
+     */
+    public boolean getEtat() {
+        return this.etat;
+    }
+
+    /**
+     * Retourne une représentation visuelle de l'état de la cellule.
+     *
+     * @return "X" si la cellule est allumée, "O" si elle est éteinte.
+     */
+    @Override
+    public String toString() {
+        return this.etat ? "X" : "O";
+    }
 }
+
+    
--- LightOff_BATCHOURINE_BRILLET_version_console/src/lightoff_batchourine_brillet_version_console/LightOff_BATCHOURINE_BRILLET_version_console.java
+++ LightOff_BATCHOURINE_BRILLET_version_console/src/lightoff_batchourine_brillet_version_console/LightOff_BATCHOURINE_BRILLET_version_console.java
@@ -14,7 +14,30 @@ public class LightOff_BATCHOURINE_BRILLET_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Création de deux cellules lumineuses
+        CelluleLumineuse cellule1 = new CelluleLumineuse();
+        CelluleLumineuse cellule2 = new CelluleLumineuse();
+
+        // Affichage de l'état initial
+        System.out.println("État initial de cellule1 : " + cellule1); // Devrait afficher "O"
+        System.out.println("État initial de cellule2 : " + cellule2); // Devrait afficher "O"
+
+        // Activation de cellule1
+        cellule1.activerCellule();
+        System.out.println("Après activation de cellule1 : " + cellule1); // Devrait afficher "X"
+
+        // Éteindre cellule2 (déjà éteinte)
+        cellule2.eteindreCellule();
+        System.out.println("Après tentative d'extinction de cellule2 : " + cellule2); // Devrait afficher "O"
+
+        // Vérification de l'état
+        System.out.println("Cellule1 est éteinte ? " + cellule1.estEteint()); // Devrait afficher false
+        System.out.println("Cellule2 est éteinte ? " + cellule2.estEteint()); // Devrait afficher true
+
+        // Réactivation et vérification
+        cellule1.activerCellule();
+        System.out.println("Après réactivation de cellule1 : " + cellule1); // Devrait afficher "O"
     }
-    
 }
+    
+
