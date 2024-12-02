c472d8527ae30d7dd6beab4c833415a39aa2454f
sachabatchourine <sachabatchourine@localhost>
2024-12-02 11:44:04 +0100

    V3 ajustement boutons avec Jpanel 2

--- LightOff_BATCHOURINE_BRILLET_version_console/src/FenetrePrincipale.java
+++ LightOff_BATCHOURINE_BRILLET_version_console/src/FenetrePrincipale.java
@@ -8,7 +8,7 @@ import javax.swing.JButton;
 
 /**
  *
- * @author baptistebrillet sacha batchourine
+ * @author baptistebrillet sachabatchourine
  */
 public class FenetrePrincipale extends javax.swing.JFrame {
     /**
